9f68d69ec7f18dcf9dc0fe7fdb7d6d4f71f0d857
shikeyu <15691157@qq.com>
2026-02-09 14:16:53 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -10,7 +10,7 @@ REM We create a dummy 'lib' file to satisfy the broken psyq.ini
 if not exist lib type nul > lib
 
 REM Use -v to see what's happening
-ccpsx -O2 -Xo$80100000 main.c -omain.cpe,main.sym,mem.map -llibapi.lib -llibgpu.lib -llibgte.lib -llibetc.lib -llibcd.lib -llibpress.lib
+ccpsx -O2 -Xo$801B0000 main.c -omain.cpe,main.sym,mem.map -llibapi.lib -llibgpu.lib -llibgte.lib -llibetc.lib -llibcd.lib -llibpress.lib
 
 if exist main.cpe (
     echo Converting to EXE...
@@ -20,3 +20,4 @@ if exist main.cpe (
     echo Build Failed!
 )
 pause
+
